2ee07d1dc2386d9bc170051be76533da11a9d005
Tayden Louw <tayden@bbd.co.za>
2018-12-14 14:12:10 +0200

    hash user password before saving

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ require('./config/config');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 
@@ -26,12 +27,30 @@ app.post('/todos', (req, res) => {
     });
 } );
 
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body,'email', 'password');
+    var user = new User(body);
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token)=>{
+        res.header('x-auth', token).send(user.toJSON());
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
+} );
+
 app.get('/todos', (req,res) => {
     Todo.find().then((todos) => {
         res.status(200).send({todos});
     }).catch((error) => {
         res.status(400).send(error);
     });
+});
+
+
+
+app.get('/users/me', authenticate, (req,res) => {
+    res.send(req.user);
 });
 
 app.get('/todos/:id', (req,res) => {
