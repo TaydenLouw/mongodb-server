4c9d883b4c2cf0b0d5a4f63f17dd35c7679761a7
Tayden Louw <tayden@bbd.co.za>
2018-12-14 10:06:54 +0200

    changed a wrong case import statement

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
-var {User} = require('./models/User');
+var {User} = require('./models/user');
 
 var app = express();
 
