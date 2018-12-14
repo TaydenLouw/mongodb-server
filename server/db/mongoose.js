7a9e5dabc2dd0f9ece491f4dddf3b649dea740c5
Tayden Louw <tayden@bbd.co.za>
2018-12-14 10:00:20 +0200

    added mlab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://Localhost:27017/TodoApp',{useNewUrlParser: true});
+mongoose.connect('mongodb://test:test123@ds159782.mlab.com:59782/tayden-todo-app',{useNewUrlParser: true});
 
 module.exports.mongoose = mongoose;
--- server/server.js
+++ server/server.js
@@ -47,8 +47,8 @@ app.get('/todos/:id', (req,res) => {
 });
 
 
-app.listen(3000, () => {
-    console.log('Started on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Started on port ${process.env.PORT || 3000}`);
 });
 
 module.exports = {app};
